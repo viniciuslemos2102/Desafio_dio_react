d5fbf0795e8f4a8344ffecf6114b31a4ec7674cb
viniciuslemos2102 <viniciuslemoscontato21@gmail.com>
2024-01-23 18:31:28 -0300

    feito a primeira parte

---
+++ src/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100%vh;
+    background-color: #cacaca;
+
+    display: flex,
+    alignItems: center,
+    justifyContent: center
+`
+export const Content  = styled.div`
+    background-color: #fafafa;
+    width: 80%;
+    min-height: 350px;
+
+`
